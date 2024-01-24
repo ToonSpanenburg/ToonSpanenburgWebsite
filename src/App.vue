20d1b426817f7960a87bbaf0976f84de1f396d8e
ToonSpanenburg <r0766397@student.thomasmore.be>
2024-01-24 01:59:41 +0100

    dropdown for small screens

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
 
+import MyHeader from "@/components/MyHeader.vue";
+import TestComponent from "@/components/TestComponent.vue";
 </script>
 
 <template>
-<h1>
-  Home
 
-</h1>
+  <MyHeader/>
+  <TestComponent/>
 </template>
 
-<style scoped>
 
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Urbanist&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+  font-family: 'Urbanist', sans-serif;
+}
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -3,9 +3,130 @@
 </script>
 
 <template>
+  <div v-if="windowWidth > 768">
+    <div class="m-4 pt-4">
+      <div class="container justify-content-center text-center">
+        <div class="row">
+          <div class="col navItem navLine">
+            <a href="#voorstelling" class="navText">Voorstelling</a>
+          </div>
+          <div class="col navItem navLine">
+            <a href="#Interesses" class="navText">interesses</a>
+          </div>
+          <div class="col navItem navLine">
+            <a href="#Ervaring" class="navText">Ervaring</a>
+          </div>
+          <div class="col navItem navLine">
+            <a href="#Skills" class="navText">Skills</a>
+          </div>
+          <div class="col navItem">
+            <a href="#Contact" class="navText">Contact</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <div class="navbar hamburger m-5 p-2">
+
+      <button class="btn btn-lg btnHeader" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse mt-2" id="collapseExample">
+        <div class="row">
+          <div class="col">
+            <ul>
+              <li>
+                <a href="#voorstelling" class="navText">Voorstelling</a>
+              </li>
+              <li>
+                <a href="#Interesses" class="navText">interesses</a>
+              </li>
+              <li>
+                <a href="#Ervaring" class="navText">Ervaring</a>
+              </li>
+              <li>
+                <a href="#Skills" class="navText">Skills</a>
+              </li>
+              <li>
+                <a href="#Contact" class="navText">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </template>
 
-<style scoped>
 
+<script>
+export default {
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+    };
+  },
+  mounted() {
+    // Add an event listener to update windowWidth on resize
+    window.addEventListener('resize', this.handleResize);
+  },
+  beforeDestroy() {
+    // Remove the event listener when the component is destroyed
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.windowWidth = window.innerWidth;
+    },
+  },
+};
+</script>
+
+<style scoped>
+a:link {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+a:active {
+  text-decoration: none;
+}
+
+.navText
+{
+  color: #123d67;
+}
+
+.navItem
+{
+  text-decoration: none;
+  padding: 5px;
+  font-size: 35px;
+  color: #123d67;
+}
+.navLine
+{
+  border-right: solid #d6c389;
+}
+
+.hamburger
+{
+  border: solid #d6c389;
+  padding: 0px;
+}
+
+.btnHeader
+{
+  margin-right: 100%;
+}
 </style>
